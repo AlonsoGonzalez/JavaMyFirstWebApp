97d7fccb03608407dddf2f1efccec2e48b6e607a
AlonsoGonzalez <alomsoglez@gmail.com>
2017-10-26 14:22:59 -0600

    este es el primer mensaje para Teachers.java

---
+++ Teachers.java
@@ -0,0 +1,20 @@
+package generation.escolar;
+
+import java.util.Date;
+
+
+
+public class Teachers 
+{
+	private String strName;
+	private int age;
+	private String CURP;
+	private String strEmail;
+	private Date dBirthDate;
+	private 
+	private
+	private
+	private
+	
+
+}
