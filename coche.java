0a0f2aec7576f52175b50d318db9e985b82d7912
AlonsoGonzalez <alomsoglez@gmail.com>
2017-10-26 14:13:58 -0600

    este es el primer mensaje para coche.java

---
+++ coche.java
@@ -0,0 +1,20 @@
+import java.util.*;
+package coche;
+
+public class coche {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+public class Car
+{
+	private String sBrand;
+	private String sType;
+	private int iVelocity;
+	
+}
